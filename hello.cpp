31ccf960e0245ee48badaa529fdf29175270a1fe
dohyun <dohyun.nam@gmail.com>
2021-04-09 09:45:05 +0900

    test111

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class C1
+{
+    int value = 7;
+public:
+    void Prt(ostream& out)
+    {
+        out << value;
+    }
+}
+
+int main() 
+{
+    C1 c;
+    ostream out;
+    c.Prt(out);
+
+    cout << out.out();
+    cout << "modify 2" << endl;
+    return 0;
+}
